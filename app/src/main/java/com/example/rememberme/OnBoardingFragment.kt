c2ec935b02e4aeed0f1aa9865b80b39aeddeb94c
Javier Santiago Salazar <100961042+JavierSantiagoSalazar@users.noreply.github.com>
2023-09-27 10:16:44 -0500

    MenuFragment creation and view finished, OnBoarding adjustments (#2)

--- app/src/main/java/com/example/rememberme/OnBoardingFragment.kt
+++ app/src/main/java/com/example/rememberme/OnBoardingFragment.kt
@@ -1,20 +1,22 @@
 package com.example.rememberme
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.rememberme.databinding.FragmentOnBoardingBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
-class OnBoarding : Fragment(R.layout.fragment_on_boarding), ViewPagerOnClickListener {
+class OnBoardingFragment : Fragment(R.layout.fragment_on_boarding) {
 
     private lateinit var mBinding: FragmentOnBoardingBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val boardList = BoardListProvider.getData(requireContext())
-        val adapter = OnBoardingViewPagerAdapter(boardList, this)
+        val adapter = OnBoardingViewPagerAdapter(boardList, ::onClick)
         mBinding = FragmentOnBoardingBinding.bind(view).apply {
             viewPager.adapter = adapter
         }
@@ -26,7 +28,8 @@ class OnBoarding : Fragment(R.layout.fragment_on_boarding), ViewPagerOnClickList
         }.attach()
     }
 
-    override fun onClick() {
-        // TODO: Navigation
+    private fun onClick() {
+        val action = OnBoardingFragmentDirections.actionOnBoardingToMenu()
+        findNavController().navigate(action)
     }
 }
--- app/src/main/java/com/example/rememberme/ViewPagerOnClickListener.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rememberme
-
-interface ViewPagerOnClickListener {
-
-    fun onClick()
-}

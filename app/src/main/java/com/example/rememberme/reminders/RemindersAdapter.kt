ecbbf145721df8d357ca5e9fed7c529b6e370f23
Javier Santiago <javiersantiago45011@gmail.com>
2023-10-10 12:59:32 -0500

    RemindersFragment, RemindersAdapter and Reminders View creation and extensions functions to diffUtil and inflate the fragment.

---
+++ app/src/main/java/com/example/rememberme/reminders/RemindersAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.rememberme.reminders
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rememberme.R
+import com.example.rememberme.Reminder
+import com.example.rememberme.basicDiffUtil
+import com.example.rememberme.databinding.ItemReminderBinding
+import com.example.rememberme.inflate
+
+class RemindersAdapter :
+    ListAdapter<Reminder, RemindersAdapter.ViewHolder>(basicDiffUtil { old, new -> old.id == new.id }) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.inflate(R.layout.item_reminder)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val reminder = getItem(position)
+        holder.bind(reminder)
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ItemReminderBinding.bind(view)
+        fun bind(reminder: Reminder) = with(binding) {
+            tvReminder.text = reminder.reminder
+            tvHour.text = reminder.hour
+            tvDate.text = reminder.date
+            tvAlarm.text = reminder.alarm
+            tvImportance.text = reminder.importance
+        }
+    }
+}

c1bccaf1c21a6169bf57af2e103684c5ef356c8e
Javier Santiago <javiersantiago45011@gmail.com>
2023-10-10 13:38:54 -0500

    Added modules: data, domain, usecases; jvm and java compile options upgraded to version_17 in all modules.

--- app/src/main/java/com/example/rememberme/MainActivity.kt
+++ app/src/main/java/com/example/rememberme/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.rememberme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.rememberme.databinding.ActivityMainBinding
---
+++ data/src/main/java/com/example/rememberme/data/MyClass.kt
@@ -0,0 +1,4 @@
+package com.example.rememberme.data
+
+class MyClass {
+}
